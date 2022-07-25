a0940a76b9f862f2d64bd4fd5082aff8776e039d
Shaurya15062008 <109532983+Shaurya15062008@users.noreply.github.com>
2022-07-25 19:54:34 +0530

    Add files via upload

--- CLASS C-12 PROJECT.js
+++ CLASS C-12 PROJECT.js
@@ -12,12 +12,12 @@ function setup(){
   //create a trex sprite
  trex = createSprite(50,180,20,50);
  trex.addAnimation('running',trex_running);
- trex.scale=0.6;
+ trex.scale=0.5;
 
  ground = createSprite(200,180,400,20);
  ground.addImage("ground",groundImage);
  ground.x = ground.width /2;
- ground.velocityX = -10;
+ ground.velocityX = -6;
 
  invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
@@ -27,7 +27,7 @@ function draw(){
   background("white")
   //make trex jump
   if(keyDown('space') && trex.y >=150){
-    trex.velocityY= -5
+    trex.velocityY= -10
   }
 
 console.log(trex.y)
